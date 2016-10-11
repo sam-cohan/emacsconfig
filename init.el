a1ac46612a3d1748fbdfcf11b5c7d9935bd3fab7
Sam Cohan <sam@ip-192-168-1-102.ec2.internal>
2016-10-10 17:13:40 -0700

    Make rainbow-delimiter color stand out more

--- init.el
+++ init.el
@@ -1030,6 +1030,13 @@
 ;; Rainbow-delimiters: highlight parentheses in rainbow colors.
 ;;
 (require 'rainbow-delimiters)
+(require 'cl-lib)
+(require 'color)
+(cl-loop
+ for index from 1 to rainbow-delimiters-max-face-count
+ do
+ (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+   (cl-callf color-saturate-name (face-foreground face) 30)))
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
 ;;
